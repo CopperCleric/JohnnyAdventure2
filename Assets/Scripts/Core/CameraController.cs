3b36d2184342d4ae09f00cb183b7d4e7d2087231
Rafid243 <rafidsaadman24@gmail.com>
2024-05-29 02:02:11 +0800

    Updated Camera
    Camera was bugged. Fixed it.

--- Assets/Scripts/Core/CameraController.cs
+++ Assets/Scripts/Core/CameraController.cs
@@ -11,16 +11,23 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private float aheadDistance;
     [SerializeField] private float cameraSpeed;
+    [SerializeField] private float minYPosition;
     private float lookAhead;
+
+   
+    private Vector3 targetPosition;
+    private Vector3 smoothVelocity;
 
     private void Update()
     {
-        //Room camera
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+        // Calculate the target position for the camera
+        targetPosition = new Vector3(player.position.x + lookAhead, Mathf.Max(player.position.y, minYPosition), transform.position.z);
 
-        //Follow player
-        //transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
-        //lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+        // Smoothly move the camera towards the target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref smoothVelocity, cameraSpeed);
+
+        // Update lookAhead to adjust the horizontal position of the camera
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
     }
 
     public void MoveToNewRoom(Transform _newRoom)
